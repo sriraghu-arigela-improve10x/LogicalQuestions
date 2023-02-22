a7a1a34fd6c4673c71c9efdd0df370a3fb783685
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-02-22 11:17:34 +0530

    create new Activity DivisibleActivity

---
+++ app/src/main/java/com/improve10x/logicalquestions/DivisibleActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.logicalquestions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DivisibleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_divisible);
+    }
+}
