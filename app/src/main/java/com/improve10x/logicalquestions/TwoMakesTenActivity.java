2c571c6bc8f588393bcc8218e0c54c6fba32376a
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-02-23 15:13:23 +0530

    implemented TwoMakesTenActivity and create XML design

--- app/src/main/java/com/improve10x/logicalquestions/TwoMakesTenActivity.java
+++ app/src/main/java/com/improve10x/logicalquestions/TwoMakesTenActivity.java
@@ -21,4 +21,15 @@ public class TwoMakesTenActivity extends AppCompatActivity {
 
     private void findTwoMakesTen() {
     }
+
+    public static boolean findTwoMakesTen(int a, int b) {
+        boolean result = false;
+        if (a == 10 || b == 10 || a + b == 10) {
+            result = true;
+        } else {
+            result = false;
+        }
+        System.out.println(result);
+        return result;
+    }
 }
