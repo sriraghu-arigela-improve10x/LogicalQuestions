aaaff9db7d921cfdda75526329d33f63a09cb06e
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-02-23 10:59:42 +0530

    create EqualityOfThreeValuesActivity

---
+++ app/src/main/java/com/improve10x/logicalquestions/EqualityOfThreeValuesActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.logicalquestions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EqualityOfThreeValuesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eqality_of_three_values);
+    }
+}
--- app/src/main/java/com/improve10x/logicalquestions/LessThenHundredActivity.java
+++ app/src/main/java/com/improve10x/logicalquestions/LessThenHundredActivity.java
@@ -20,5 +20,26 @@ public class LessThenHundredActivity extends AppCompatActivity {
     }
 
     private void findTheLessThenHundred() {
+        binding.findBtn.setOnClickListener(v -> {
+            String number1 = binding.number1Txt.getText().toString();
+            int num1 = Integer.parseInt(number1);
+            String number2 = binding.number2Txt.getText().toString();
+            int num2 = Integer.parseInt(number2);
+            boolean result = lessThan100(num1,num2);
+            String value = String.valueOf(result);
+            binding.lessthenTxt.setText(value);
+        });
+    }
+
+    public static boolean lessThan100(int a, int b) {
+        boolean result = false;
+        int sum = a + b;
+        if (sum < 100) {
+            result = true;
+        } else {
+            result = false;
+        }
+        System.out.println(result);
+        return result;
     }
 }
