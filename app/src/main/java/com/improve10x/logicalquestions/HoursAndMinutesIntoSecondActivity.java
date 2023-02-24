076c56e9329dafcb51f817a52083fd9316084222
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-02-24 14:15:27 +0530

    create HoursAndMinutesIntoSecondActivity

---
+++ app/src/main/java/com/improve10x/logicalquestions/HoursAndMinutesIntoSecondActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.logicalquestions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoursAndMinutesIntoSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hours_and_minutes_into_second);
+    }
+}
