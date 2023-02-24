a5f365525b747776175369402ac4cc9061a902d2
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-02-24 11:47:10 +0530

    create MinutesInToSecondsActivity

---
+++ app/src/main/java/com/improve10x/logicalquestions/MinutesInToSecondsActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.logicalquestions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MinutesInToSecondsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minutes_in_to_seconds);
+    }
+}
